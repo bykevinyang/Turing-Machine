764bafa20c49d48d6eb5e6ae92507413b41ff7b0
Kevin Yang <>
2021-06-07 02:27:57 +0000

    Test

---
+++ TTuple.swift
@@ -0,0 +1,40 @@
+enum Direction{  
+ case left 
+ case right  
+}
+
+struct TTuple : CustomStringConvertible{  
+ let currentState: Int  
+ let inputChar: Character  
+ let newState: Int  
+ let outputChar: Character  
+ let direction: Direction  
+
+init(currentState:Int, inputChar: Character, newState: Int, outputChar: Character, direction: Direction){
+
+  self.currentState = currentState
+  self.inputChar = inputChar
+  self.newState = newState
+  self.outputChar = outputChar
+  self.direction = direction
+} 
+ static func makeKey(_ state: Int, _ inputChar: Character)->String  {
+   return String(state) + " " + String(inputChar)
+ }
+
+ func getKey()->String{
+   return String(currentState) + " " + String(inputChar)
+ }
+
+ var description: String{
+   return "cs: \(String(currentState)), ic: \(String(inputChar)), ns: \(String(newState)), oc: \(String(outputChar)), di: \(direction)"
+ } 		//prints out TTuples with labels: cs, ic, ns, oc, di
+
+ func nakedTuple()->String{
+   var str = String(currentState) + String(inputChar) + String(newState) + String(outputChar)
+  str = str.map { "\($0)" }.joined(separator: " ")
+  str += " \(direction)"
+   return str
+ }
+   	//prints out TTuples without labels; “naked”
+}  
--- main.swift
+++ main.swift
@@ -1,17 +1,4 @@
-enum Direction{  
- case left 
- case right  
-}  
-struct TTuple : CustomStringConvertible{  
- let currentState: Int  
- let inputChar: Character  
- let newState: Int  
- let outputChar: Character  
- let direction: Direction  
-
-init(currentState:Int, inputChar: Character, newState: Int, outputChar: Character, direction: Direction) 
- static func makeKey(state: Int, inputChar: Character)->String  
- func getKey()->String  
- var description: String  		//prints out TTuples with labels: cs, ic, ns, oc, di
- func nakedTuple()->String  	//prints out TTuples without labels; “naked”
-}  
+let test = TTuple(currentState: 1, inputChar: "d", newState: 2, outputChar: "3", direction: Direction.right)
+print(test)
+print(test.getKey())
+print(test.nakedTuple())
